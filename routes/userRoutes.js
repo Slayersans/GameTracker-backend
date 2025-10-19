0c2ffc086af3aae73c00757920787691d33f6d83
Slayersans <jeisonsossa88@gmail.com>
2025-10-18 19:41:57 -0500

    Add user logic
    Created User model schema
    Implemented user controller with basic CRUD logic
    Added user route and linked it to the Express server

---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const routes = express.Router();
+const userController = require('../controllers/userController');
+
+routes.post('/register', userController.registerUser);
+routes.post('/login', userController.loginUser);
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => { res.send('API esta funcionando'); });
+app.use('/api/users', require('./routes/userRoutes'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
